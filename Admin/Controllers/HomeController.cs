e3ea72dc8e8dfd36fd6b13d779e33f5d9fd7e073
LeMinhThuc <leminhthuc1882005@gmail.com>
2025-12-12 17:40:33 +0700

    Start

---
+++ Admin/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        G6_QLBQAEntities db = new G6_QLBQAEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<SanPham> lstSP = db.SanPham.ToList();
+            return View(lstSP);
+        }
+    }
+}
---
+++ Admin/Models/AnhSP.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Admin.Models
+{
+	public class AnhSP
+	{
+        [Key, Column(Order = 0)]
+        public int MASP { get; set; }
+        [Key, Column(Order = 1)]
+        public string ANH { get; set; }
+        public string TENANH { get; set; }
+
+
+        public virtual SanPham SANPHAM { get; set; }
+    }
+}
