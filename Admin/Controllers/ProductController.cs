ba792eda7cf9c9b2d34317339c0402d3365282e3
Shind <duyt7416@gmail.com>
2025-12-12 23:09:06 +0700

    ChiTietSanPham

---
+++ Admin/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        G6_QLBQAEntities db = new G6_QLBQAEntities();
+        // GET: Product
+        public ActionResult ChiTietSP(int masp)
+        {
+            var sp = db.SanPham.FirstOrDefault(s => s.masp == masp);
+            //sản phẩm liên quan
+            List<SanPham> lienquan = db.SanPham.Where(s => s.madm == sp.madm || s.maloai == sp.maloai || s.math == sp.math && s.masp != masp).ToList();
+            ViewBag.LQ = lienquan;
+            var thuongHieu = db.ThuongHieu.FirstOrDefault(t => t.math == sp.math);
+            ViewBag.TH = thuongHieu.tenth;
+            return View(sp);
+        }
+    }
+}
