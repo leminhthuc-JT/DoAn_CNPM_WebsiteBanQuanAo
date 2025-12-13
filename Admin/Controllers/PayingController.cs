b2e76fd59de20e2b455cf82f476e202b85254085
LeMinhThuc <leminhthuc1882005@gmail.com>
2025-12-13 10:58:02 +0700

    detail - image - size - quantity - description

---
+++ Admin/Controllers/PayingController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Controllers
+{
+    public class PayingController : Controller
+    {
+        // GET: Paying
+        private G6_QLBQAEntities db = new G6_QLBQAEntities();
+        public ActionResult Index(int matk, int masp, int mam, int mas, int quantity)
+        {
+            SanPham sp = db.SanPham.Where(x => x.masp == masp).FirstOrDefault();
+            TaiKhoan tk = db.TaiKhoan.Where(x => x.matk == matk).FirstOrDefault();
+            ViewBag.SL = quantity;
+            ViewBag.Mau = mam;
+            ViewBag.Size = mas;
+            ViewBag.Gia = sp.giaban * quantity;
+            return View();
+        }
+    }
+}
