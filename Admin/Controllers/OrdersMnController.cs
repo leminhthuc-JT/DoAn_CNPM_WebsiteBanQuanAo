ce25308f538f961fce422e94a57a5c487f563c46
meowdev5s <letuanhoi17092005@gmail.com>
2025-12-14 22:13:26 +0700

    admin
    voucher, order

---
+++ Admin/Controllers/OrdersMnController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Controllers
+{
+    public class OrdersMnController : Controller
+    {
+        G6_QLBQAEntities db = new G6_QLBQAEntities();
+
+        //DANH SÁCH ĐƠN HÀNG
+        public ActionResult Index()
+        {
+            var orders = db.HoaDon
+                .OrderByDescending(x => x.ngaylap)
+                .ToList();
+
+            return View(orders);
+        }
+
+        //CHUYỂN TRẠNG THÁI
+        [HttpPost]
+        public ActionResult UpdateStatus(int id, string nextStatus)
+        {
+            var order = db.HoaDon.Find(id);
+            if (order == null)
+                return RedirectToAction("Index");
+
+            // KHÓA LOGIC – KHÔNG CHO QUAY NGƯỢC
+            if (order.tinhtrang == "Chờ xác nhận")
+            {
+                if (nextStatus == "Đã xác nhận" || nextStatus == "Đã hủy")
+                    order.tinhtrang = nextStatus;
+            }
+            else if (order.tinhtrang == "Đã xác nhận")
+            {
+                if (nextStatus == "Chờ giao hàng")
+                    order.tinhtrang = nextStatus;
+            }
+            else if (order.tinhtrang == "Chờ giao hàng")
+            {
+                if (nextStatus == "Đã hoàn thành")
+                    order.tinhtrang = nextStatus;
+            }
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //HỦY ĐƠN (CHỈ KHI CHỜ XÁC NHẬN)
+        [HttpPost]
+        public ActionResult Cancel(int id)
+        {
+            var hd = db.HoaDon.Find(id);
+            if (hd == null) return HttpNotFound();
+
+            if (hd.tinhtrang == "Chờ xác nhận")
+            {
+                hd.tinhtrang = "Đã hủy";
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        //XÁC NHẬN ĐÃ THANH TOÁN
+        [HttpPost]
+        public ActionResult XacNhanThanhToan(int id)
+        {
+            var hd = db.HoaDon.Find(id);
+            if (hd == null) return HttpNotFound();
+
+            hd.dathanhtoan = true;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Admin/Controllers/VouchersMnController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Controllers
+{
+    public class VouchersMnController : Controller
+    {
+        G6_QLBQAEntities db = new G6_QLBQAEntities();
+
+        //DANH SÁCH 
+        public ActionResult Index()
+        {
+            var list = db.GiamGia.OrderByDescending(x => x.ngaybd).ToList();
+            return View(list);
+        }
+
+        //THÊM 
+        public ActionResult Create()
+        {
+            return View(new GiamGia());
+        }
+
+        [HttpPost]
+        public ActionResult Create(GiamGia model)
+        {
+            if (model.mucgiam > 99)
+            {
+                ModelState.AddModelError("", "Mức giảm không được vượt quá 99%");
+            }
+
+            if (model.ngaybd <= DateTime.Today)
+            {
+                ModelState.AddModelError("", "Ngày bắt đầu không được nhỏ hơn ngày hiện tại");
+            }
+
+            if (model.ngaykt <= DateTime.Today)
+            {
+                ModelState.AddModelError("ngaykt", "Ngày kết thúc phải lớn hơn ngày hiện tại");
+            }
+
+            if (model.ngaykt <= model.ngaybd)
+            {
+                ModelState.AddModelError("ngaykt", "Ngày kết thúc phải sau ngày bắt đầu");
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.GiamGia.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        //SỬA
+        public ActionResult Edit(int id)
+        {
+            var gg = db.GiamGia.Find(id);
+            if (gg == null) return HttpNotFound();
+            return View(gg);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(GiamGia model)
+        {
+            if (model.mucgiam > 99)
+            {
+                ModelState.AddModelError("", "Mức giảm không được vượt quá 99%");
+            }
+
+            if (model.ngaybd < DateTime.Today)
+            {
+                ModelState.AddModelError("", "Ngày bắt đầu không được nhỏ hơn ngày hiện tại");
+            }
+
+            if (model.ngaykt <= DateTime.Today)
+            {
+                ModelState.AddModelError("ngaykt", "Ngày kết thúc phải lớn hơn ngày hiện tại");
+            }
+
+            if (model.ngaykt <= model.ngaybd)
+            {
+                ModelState.AddModelError("ngaykt", "Ngày kết thúc phải sau ngày bắt đầu");
+            }
+
+            if (ModelState.IsValid)
+            {
+                var gg = db.GiamGia.Find(model.magg);
+                gg.mucgiam = model.mucgiam;
+                gg.ngaybd = model.ngaybd;
+                gg.ngaykt = model.ngaykt;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        //XÓA
+        public ActionResult Delete(int id)
+        {
+            var gg = db.GiamGia.Find(id);
+            if (gg != null)
+            {
+                db.GiamGia.Remove(gg);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
